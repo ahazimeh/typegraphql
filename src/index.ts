f2d008dc6dff185e810dbac51731ec9013745b42
ahazimeh01 <ali.h@echovalley.io>
2023-03-17 23:33:00 +0200

    register resolver typegraphql

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,41 @@ import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import * as Express from "express";
 import { buildSchema } from "type-graphql";
-import { Resolver, Query } from "type-graphql";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
 
-@Resolver()
-class HelloResolver {
-  @Query(() => String, { name: "helloWorld" })
-  async hello() {
-    return "Hello World!";
-  }
-}
+import { RegisterResolver } from "./modals/user/register";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "postgres",
+  database: "mydb",
+  synchronize: true,
+  logging: true,
+  entities: [User],
+  subscribers: [],
+  migrations: [],
+});
 
 const main = async () => {
   const schema = await buildSchema({
-    resolvers: [HelloResolver],
+    resolvers: [RegisterResolver],
   });
   const apolloServer = new ApolloServer({ schema });
 
   const app = Express();
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
+
+  AppDataSource.initialize()
+    .then(() => {
+      console.log("aa");
+      // here you can start to work with your database
+    })
+    .catch((error) => console.log(error));
 
   app.listen(4000, () => {
     console.log("Server started on http://localhost:4000");
